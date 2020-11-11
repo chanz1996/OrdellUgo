8c52ff54bc4ee1e40f31990c5a1a7b44a7eb6689
chandini <chanz1996@users.noreply.github.com>
2020-11-12 00:30:15 +0530

    Add files via upload

---
+++ facedetect/face_detect.py
@@ -0,0 +1,34 @@
+import cv2
+#import sys
+
+# Get user supplied values
+imagePath = 'abba.png'
+cascPath = 'haarcascade_frontalface_default.xml'
+
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+# Read the image
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Detect faces in the image
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    #flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+)
+
+print "Found {0} faces!".format(len(faces))
+
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+cv2.imshow("Faces found", image)
+cv2.waitKey(0)
+#image = image[c1:c1+25,r1:r1+25]
+cv2.imshow("Faces1" , image)
+#cv2.imwrite('faces1.png',image)
+
